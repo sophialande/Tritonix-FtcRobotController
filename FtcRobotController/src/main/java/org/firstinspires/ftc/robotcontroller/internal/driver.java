b144df8672783e0f0a207772bcdc9d219a1fc9d2
Noah-MG <noah.glimcher28@avenues.org>
2024-10-07 16:19:18 -0400

    Added telem class and driver class. Noah made these over the weekend. Telemetry allows something to permanently be displayed to the telemetry list. Driver can do rotation, and driving in any direction given an angle. All the conversion might not be right and the direction of the driving might not be properly normalized.

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/driver.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class driver {
+
+    static DcMotor fr = hardwareMap.get(DcMotor.class, "fr");
+    static DcMotor fl = hardwareMap.get(DcMotor.class, "fl");
+    static DcMotor br = hardwareMap.get(DcMotor.class, "br");
+    static DcMotor bl = hardwareMap.get(DcMotor.class, "bl");
+
+    public static void drive(double speed, double in, double degrees) {
+
+        degrees = -degrees;
+
+        // Reset the tick encoders to zero
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int ticks = (int) (in * 8.753);
+
+        double radians = degrees * PI / 180;
+
+        double frblMultiplier = cos(radians)-sin(radians);
+        double flbrMultiplier = cos(radians)+sin(radians);
+
+        int frblTicks = (int) (ticks * frblMultiplier);
+        int flbrTicks = (int) (ticks * flbrMultiplier);
+
+        // set the target position
+        fr.setTargetPosition(frblTicks);
+        fl.setTargetPosition(flbrTicks);
+        br.setTargetPosition(flbrTicks);
+        bl.setTargetPosition(frblTicks);
+
+        // Change the mode to spin until reaching the position
+        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // make the motors run at the given speed
+        fr.setPower(speed * frblMultiplier);
+        fl.setPower(speed * flbrMultiplier);
+        br.setPower(speed * flbrMultiplier);
+        bl.setPower(speed * frblMultiplier);
+
+        // Run while both motors are moving
+        while (fr.isBusy()) {
+
+            // Update the telem data
+            telemetry.addData("Running to", "Font Right and Back Left: " + frblTicks + " | Front Left and Back Right: " + flbrTicks);
+            telemetry.addData("Current pos", "Front Right: " + fr.getCurrentPosition() + " | Front Left: " + fl.getCurrentPosition() + " | Back Right: " + br.getCurrentPosition() + " | Back Left: " + bl.getCurrentPosition());
+            telem.update();
+        }
+
+        // Stop the motors
+        fr.setPower(0);
+        fl.setPower(0);
+        br.setPower(0);
+        bl.setPower(0);
+
+    }
+
+    public static void rotate(double speed, double degrees){
+
+        // Reset the tick encoders to zero
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int ticks = (int) (degrees * 15.6);
+
+        // set the target position
+        fr.setTargetPosition(-ticks);
+        fl.setTargetPosition(ticks);
+        br.setTargetPosition(-ticks);
+        bl.setTargetPosition(ticks);
+
+        // Change the mode to spin until reaching the position
+        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // make the motors run at the given speed
+        fr.setPower(-speed);
+        fl.setPower(-speed);
+        br.setPower(-speed);
+        bl.setPower(-speed);
+
+        // Run while both motors are moving
+        while (fr.isBusy()) {
+            // Update the telem data
+            telemetry.addData("Running to", "Left " + ticks + " | Right: " + -ticks);
+            telemetry.addData("Current pos", "Front Right: " + fr.getCurrentPosition() + " | Front Left: " + fl.getCurrentPosition() + " | Back Right: " + br.getCurrentPosition() + " | Back Left: " + bl.getCurrentPosition());
+            telem.update();
+        }
+
+        // Stop the motors
+        fr.setPower(0);
+        fl.setPower(0);
+        br.setPower(0);
+        bl.setPower(0);
+    }
+}
---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/telem.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* USAGE GUIDE:
+ * The purpose of this class is to introduce the capability to include data in the telemetry output
+ * for more than just one update. This is useful when you would like to, for example, include a
+ * status (such as "sample in hand") while the robot is moving (when it will likely be constantly
+ * updating telemetry).
+ *
+ * To add something to the permanent telemetry list, call telem.add(a, b), imagine it is exactly
+ * like telemetry.addData() but with a different name. So a is the value and it will be printed
+ * followed by ": " and then b. Unlike in telemetry.addData() both a and b must be strings. If
+ * something is already on the list with that name (a), it will be replaced.
+ *
+ * To remove something from the permanent telemetry list, call telem.remove(a), where a is the name
+ * of the thing you would like to remove. a must be a string.
+ *
+ * WHENEVER you wish to update the telemetry, instead of using telemetry.update(), use
+ * telem.update(). Otherwise it will be updated without the permanent list. IMPORTANT: IF YOU WISH
+ * TO INCLUDE DATA THAT WILL CHANGE EVERY UPDATE, SIMPLY USE telemetry.addData(). OTHERWISE YOU
+ * WILL BE MAKING THE ROBOT RUN MUCH MORE SLOWELY, REDUCING OUR POSITION ACCURACY AND GIVING THE
+ * OPPONENT A LEG UP.
+ */
+
+public class telem {
+
+    // Creates a map, made out of a list of pairs of string, the first string is the key and the
+    // second is the value. This is searchable by the value. It will be printed as telem data.
+    public static Map<String, String> permanentTelems = new HashMap<String, String>();
+
+    // Call as telem.add("", "")
+    public static void add(String name, String value) {
+        // Adds new data to the map, if the map already has something with the same key, it will
+        // simply replace the value with the new value.
+        permanentTelems.put(name, value);
+    }
+
+    // Call as telem.remove("", "")
+    public static void remove(String name) {
+        // Removes any data only with the key
+        permanentTelems.remove(name);
+    }
+
+    // Call as telem.update("", "") and include any temp before calling
+    public static void update() {
+        // Goes through every pair in the list and adds them to the telemetry output before
+        // updating.
+        for (Map.Entry<String, String> me: permanentTelems.entrySet()) {
+            telemetry.addData(me.getKey(), me.getValue());
+        }
+        telemetry.update();
+    }
+}
