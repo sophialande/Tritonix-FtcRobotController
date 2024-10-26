70e4171c51a3324fa527f87ba89cca7012be688d
Noah-MG <noah.glimcher28@avenues.org>
2024-10-26 10:33:36 -0400

    Made ports a little more versatile

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ports.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ports.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.*;
 
 /*
  * USAGE GUIDE:
