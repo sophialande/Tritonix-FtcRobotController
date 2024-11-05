274986ce75875cfa07eace714aaf04a8f9122787
Noah-MG <noah.glimcher28@avenues.org>
2024-11-05 12:58:51 -0500

    The teleop constoller should now be capable of moving around x-y. It hasn't been tested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopOpMode.java
@@ -1,17 +1,40 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Gamepad;
+import static org.firstinspires.ftc.teamcode.Ports.*;
 
-@Config
-@TeleOp{name="TeleOp Opmode Tritonics"}
-public class TeleopOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Ports;
+
+
+@TeleOp(name = "Teleop Tritonics")
+public class TeleopOpMode extends LinearOpMode {
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
 
+        Ports.init(this);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double frblMultiplier = 0.707107*gamepad1.left_stick_x+0.707107*gamepad1.left_stick_y;
+            double flbrMultiplier = 0.707107*gamepad1.left_stick_y-0.707107*gamepad1.left_stick_x;
+
+            // make the motors run at the given speed
+            fr.setPower(frblMultiplier);
+            fl.setPower(flbrMultiplier);
+            br.setPower(flbrMultiplier);
+            bl.setPower(frblMultiplier);
+
+        }
     }
-
 }
+
+
+
+
+
+
