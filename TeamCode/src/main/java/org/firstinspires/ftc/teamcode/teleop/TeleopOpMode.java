d312a5c0cd82d74eafffcfaac79ea1737d90c277
Noah-MG <noah.glimcher28@avenues.org>
2024-11-09 17:07:37 -0500

    Updated the Teleop controller so include all of the stuff Sophia wrote incide the while loop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopOpMode.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import static org.firstinspires.ftc.teamcode.hardware.Ports.*;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -23,91 +21,87 @@ public class TeleopOpMode extends LinearOpMode {
             double max;
 
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
+            double drive = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double strafe = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
+            double fr = drive - strafe - yaw;
+            double fl = drive + strafe + yaw;
+            double br = drive + strafe - yaw;
+            double bl = drive - strafe + yaw;
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
+            max = Math.max(Math.abs(fl), Math.abs(fr));
+            max = Math.max(max, Math.abs(bl));
+            max = Math.max(max, Math.abs(br));
 
             if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+                fl /= max;
+                fr /= max;
+                bl /= max;
+                br /= max;
             }
 
-            fl.setPower(leftFrontPower);
-            fr.setPower(rightFrontPower);
-            bl.setPower(leftBackPower);
-            br.setPower(rightBackPower);
-        }
+            Ports.fl.setPower(fl);
+            Ports.fr.setPower(fr);
+            Ports.bl.setPower(bl);
+            Ports.br.setPower(br);
 
-        // VERTICAL LINEAR SLIDES (UP AND DOWN)
-        if (gamepad1.y) {
-            Driver.linearSlidesVUp(1, 3200);
-        }
-        else {
-            Driver.linearSlidesVUp(0, 3200);
-        }
 
-        if (gamepad1.a) {
-            Driver.linearSlidesVDown(-1, -1300);
-        }
-        else {
-            Driver.linearSlidesVDown(0, -1300);
-        }
+            // VERTICAL LINEAR SLIDES (UP AND DOWN)
+            if (gamepad1.y) {
+                Driver.linearSlidesVUp(1, 3200);
+            } else {
+                Driver.linearSlidesVUp(0, 3200);
+            }
 
-        // HORIZONTAL LINEAR SLIDES (IN AND OUT)
-        if (gamepad1.x) {
-            Driver.linearSlidesHUp(1, 3200);
-        }
-        else {
-            Driver.linearSlidesHUp(0, 3200);
-        }
+            if (gamepad1.a) {
+                Driver.linearSlidesVDown(-1, -1300);
+            } else {
+                Driver.linearSlidesVDown(0, -1300);
+            }
 
-        if (gamepad1.b) {
-            Driver.linearSlidesHDown(-1, -1300);
+            // HORIZONTAL LINEAR SLIDES (IN AND OUT)
+            if (gamepad1.x) {
+                Driver.linearSlidesHUp(1, 3200);
+            } else {
+                Driver.linearSlidesHUp(0, 3200);
+            }
+
+            if (gamepad1.b) {
+                Driver.linearSlidesHDown(-1, -1300);
+            } else {
+                Driver.linearSlidesHDown(0, -1300);
+            }
+
+            // CLAW
+            if (gamepad1.dpad_right) {
+                Driver.claw(1);
+            }
+            if (gamepad1.dpad_left) {
+                Driver.claw(-1);
+            }
+            //change
+
+            // INTAKE/OUTTAKE (DO LATER BECAUSE THEY HAVEN'T DECIDED YET
+
+            // HANGING MECHANISM (DO LATER BECAUSE THEY HAVEN'T DECIDED YET
+
+            //TELEMETRY
+            telemetry.addData("Front Left Pow", Ports.fl.getPower());
+            telemetry.addData("Back Left Pow", Ports.bl.getPower());
+            telemetry.addData("Front Right Pow", Ports.fr.getPower());
+            telemetry.addData("Back Right Pow", Ports.br.getPower());
+            telemetry.addData("Linear Slide Vertical Right Position", Ports.lsv_r.getCurrentPosition());
+            telemetry.addData("Linear Slide Vertical Left Position", Ports.lsv_l.getCurrentPosition());
+            telemetry.addData("Linear Slide Horizontal Right Position", Ports.lsh_r.getCurrentPosition());
+            telemetry.addData("Linear Slide Horizontal Left Position", Ports.lsh_l.getCurrentPosition());
+            //telemetry.addData("Claw position", Ports.claw.getCurrentPosition());
+            telemetry.update();
         }
-        else {
-            Driver.linearSlidesHDown(0, -1300);
-        }
-
-        // CLAW
-        if (gamepad1.dpad_right) {
-            Driver.claw(1);
-        }
-        if (gamepad1.dpad_left) {
-            Driver.claw(-1);
-        }
-        //change
-
-        // INTAKE/OUTTAKE (DO LATER BECAUSE THEY HAVEN'T DECIDED YET
-
-        // HANGING MECHANISM (DO LATER BECAUSE THEY HAVEN'T DECIDED YET
-
-        //TELEMETRY
-        telemetry.addData("Front Left Pow", fl.getPower());
-        telemetry.addData("Back Left Pow", bl.getPower());
-        telemetry.addData("Front Right Pow", fr.getPower());
-        telemetry.addData("Back Right Pow", br.getPower());
-        telemetry.addData("Linear Slide Vertical Right Position", lsv_r.getCurrentPosition());
-        telemetry.addData("Linear Slide Vertical Left Position", lsv_l.getCurrentPosition());
-        telemetry.addData("Linear Slide Horizontal Right Position", lsh_r.getCurrentPosition());
-        telemetry.addData("Linear Slide Horizontal Left Position", lsh_l.getCurrentPosition());
-        //telemetry.addData("Claw position", claw.getCurrentPosition());
-        telemetry.update();
-
     }
 }
 
