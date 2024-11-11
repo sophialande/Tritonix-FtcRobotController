3b9ac751269c7fb3578de42d7cdb6e741a4077fd
Noah-MG <noah.glimcher28@avenues.org>
2024-11-11 18:04:12 -0500

    Finished the tuning opMode, it likely will need some testing the day of, which will be a bit of a challenge not to waste to much time on.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTester.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.hardware.Ports;
+import org.firstinspires.ftc.teamcode.hardware.Telem;
 
 //Create an opmode class
 @Config
@@ -35,6 +38,11 @@ public class PIDTester extends LinearOpMode {
         }
     }
 
+    //Give the slide you want to tune here
+    DcMotor slide;
+    //and the target number of ticks here
+    int target;
+
     //Create the opmode function
     @Override
     public void runOpMode(){
@@ -51,11 +59,33 @@ public class PIDTester extends LinearOpMode {
                 } else if (!gamepad1.dpad_left && prevGamepad1.dpad_left){
                     selectedConstant--;
                 }
+
                 if(!gamepad1.dpad_up && prevGamepad1.dpad_up){
                     increaseConstant(selectedConstant, 1);
                 } else if (!gamepad1.dpad_down && prevGamepad1.dpad_down){
                     increaseConstant(selectedConstant, -1);
                 }
+
+                if(!gamepad1.x && prevGamepad1.x){
+                    pidController.Setup(slide.getCurrentPosition()-target);
+                    ElapsedTime timeElapsed = new ElapsedTime();
+                    while(gamepad1.x){
+                        slide.setPower(pidController.Evaluate(slide.getCurrentPosition()-target));
+                        telemetry.addData("Current Position", slide.getCurrentPosition());
+                        telemetry.addData("Time After Start", timeElapsed.time());
+                    }
+                } else {
+                    telemetry.addData("Kp(0)", pidController.Kp);
+                    telemetry.addData("Ki(1)", pidController.Ki);
+                    telemetry.addData("Kd(2)", pidController.Kd);
+                    telemetry.addData("Kf(3)", pidController.Kf);
+                    telemetry.addLine();
+                    telemetry.addData("Currently Editing (index)", selectedConstant);
+                    telemetry.addLine("Change constant to edit with left & right.");
+                    telemetry.addLine("Change constant with up and down.");
+                    telemetry.addLine("Press x to start the trial");
+                }
+                Telem.update(this);
             }
         }
     }
