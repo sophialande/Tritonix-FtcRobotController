75b350fe532f86d2930f6ef6b7cbea418783d9c8
Noah-MG <noah.glimcher28@avenues.org>
2024-11-11 12:16:55 -0500

    Added a class for the PID controller, usage guide coming.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PIDController.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.util.*;
+
+public class PIDController {
+
+    //Define the parameters of the PID controller
+    double Kp = 0;
+    double Ki = 0;
+    double Kd = 0;
+    double Kf = 0;
+
+    ElapsedTime elapsedTime = new ElapsedTime();
+    double integralSum = 0;
+
+    int lastPosition;
+
+    void Setup(int position){
+        lastPosition = position;
+        elapsedTime.reset();
+    }
+
+    double Evaluate(int position){
+
+        integralSum += position*elapsedTime.time();
+        double output = position*Kp + integralSum*Ki + (position-lastPosition)*Kd/elapsedTime.time() + Kf;
+        elapsedTime.reset();
+        return(output);
+
+    }
+}
