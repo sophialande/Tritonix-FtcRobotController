dcf1bc0e7390f4d1a881a802f6651fcb6ca419a0
Noah-MG <noah.glimcher28@avenues.org>
2024-12-04 16:19:04 -0500

    Finished Refactoring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/Movement.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.classes;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
